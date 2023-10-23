0386afb8afbb8d6cad8723140a5e6f4cabbefb20
Alex Hort-Francis <alex.hortfrancis@gmail.com>
2023-10-23 17:19:16 +0100

    Initial commit
    - SQL database connection established.

---
+++ inc/connection.php
@@ -0,0 +1,19 @@
+<?php 
+
+// Set database credentials 
+$server = 'localhost';
+$username = 'root';
+$password = 'pw123';
+$database_name = 'phpcrud';
+
+try {
+
+    $database_connection = new PDO("mysql:host=$server;dbname=$database_name", $username, $password);
+
+} catch (PDOException $e) {
+
+    echo 'Connection to database failed: ' . $e->getMessage();
+    exit;
+}
+
+echo "Database seems to have worked!";
